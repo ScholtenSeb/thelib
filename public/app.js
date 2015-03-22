9057baa990ef1486cdcf5f4fd995955767a026cf
ScholtenSeb <scholten.seb@gmail.com>
2015-03-22 22:31:18 +0200

    Big changes

--- public/app.js
+++ public/app.js
@@ -1,7 +1,26 @@
-var lib = angular.module('TheLib', ['ngRoute']);
+var lib = angular.module('app', ['ngRoute']);
 
-lib.controller('LibCtrl', function ($scope) {
-	$scope.test = 'Hellowad World';
+lib.controller('LibCtrl', ['$scope','$http', function ($scope, $http) {
+	
+}]);
+
+lib.controller('DocsCtrl', ['$scope', '$routeParams','$http', function ($scope, $routeParams, $http) {
+
+	var type = $routeParams.type;
+
+	$http.get('api/v1/'+type).success( function (data) {
+		$scope.docs = data;
+	});
+
+}]);
+
+lib.config( function ($routeProvider) {
+	$routeProvider.when('/docs/:type', {
+		templateUrl	: 'views/docs.html',
+		controller	: 'DocsCtrl'
+	}).otherwise({
+		redirectTo	: '/'
+	})
 });
 
 
--- server.js
+++ server.js
@@ -1,16 +1,48 @@
-var express = require('express');
+var express = require('express'); // Express module
 
+var mysql = require('mysql'); // MySQL module
+
+// Create DB connection
+var conn = mysql.createConnection({
+	host : 'dedi409.jnb1.host-h.net',
+	user : 'db_admin',
+	password : 'NhMVSm48',
+	database : 'db_thelib'
+});
+
+// Init app
 var app = express();
 
-app.use(express.static(__dirname + '/public'));     
+app.use(express.static(__dirname + '/public')); //Path config    
 
-app.get('/api', function (req, res) {
-	res.send('Hello API');
+
+// Routes - API
+app.get('/api/v1/:type?', function (req, res) {
+
+	var type = req.params.type; // Get type param from URL
+
+	// Query database
+	conn.query( 'SELECT * FROM tl_docs WHERE type = ?' , [type] , function (error, results) {
+
+		if(error) {
+			throw error; // Throw error
+		} else {
+			//console.log('Requested: '+type);
+			res.json(results); // Render results in json format
+		}
+
+	});
+
+	//conn.end(); // Close DB connection;
+	
 });
 
+// Routes - Front-End
 app.get('*', function (req, res) {
-	res.sendfile('/public/index.html');
+	res.sendfile('/public/index.html'); //Render front-end (angular)
 });
 
+
+// Server
 app.listen(3000);
 console.log('Listening on port 3000');
