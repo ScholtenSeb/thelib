9c0215a59422d806add65771a403423a8ab4ef46
ScholtenSeb <scholten.seb@gmail.com>
2015-03-24 09:42:48 +0200

    General savw

--- public/app.js
+++ public/app.js
@@ -1,26 +1,64 @@
 var lib = angular.module('app', ['ngRoute']);
 
 lib.controller('LibCtrl', ['$scope','$http', function ($scope, $http) {
+
+	// var type = $routeParams.type;
+
+	// $http.get('api/v1/'+type).success( function (data) {
+	// 	$scope.docs = data;
+	// });
 	
 }]);
 
-lib.controller('DocsCtrl', ['$scope', '$routeParams','$http', function ($scope, $routeParams, $http) {
+lib.service('getDocs', [ '$http', function ($http) {
 
-	var type = $routeParams.type;
+	var baseUrl = 'api/v1/';
 
-	$http.get('api/v1/'+type).success( function (data) {
-		$scope.docs = data;
-	});
+	this.books = $http.get(baseUrl+'book');
+	this.articles = $http.get(baseUrl+'article');
+	this.papers = $http.get(baseUrl+'paper');
 
 }]);
 
+lib.controller('BooksCtrl', ['$scope','getDocs', function ($scope,getDocs) {
+	getDocs.books.success(function(data){
+		$scope.documents = data;
+	});
+	console.log('Viewing: books')
+}]);
+
+lib.controller('ArticlesCtrl', ['$scope','getDocs', function ($scope,getDocs) {
+	getDocs.articles.success(function(data){
+		$scope.documents = data;
+	});
+	console.log('Viewing: articles')
+}]);
+
+lib.controller('PapersCtrl', ['$scope','getDocs', function ($scope,getDocs) {
+	getDocs.papers.success(function(data){
+		$scope.documents = data;
+	});
+	console.log('Viewing: papers')
+}]);
+
+
+
 lib.config( function ($routeProvider) {
-	$routeProvider.when('/docs/:type', {
-		templateUrl	: 'views/docs.html',
-		controller	: 'DocsCtrl'
+	$routeProvider.when('/', {
+		templateUrl	: 'views/home.html',
+		controller	: 'LibCtrl'
+	}).when('/books', {
+		templateUrl	: 'views/books.html',
+		controller	: 'BooksCtrl'
+	}).when('/articles', {
+		templateUrl	: 'views/articles.html',
+		controller	: 'ArticlesCtrl'
+	}).when('/papers', {
+		templateUrl	: 'views/papers.html',
+		controller	: 'PapersCtrl'
 	}).otherwise({
 		redirectTo	: '/'
-	})
+	});
 });
 
 
